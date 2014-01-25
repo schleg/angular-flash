0927800646b93548e96db580bc9ab3ceea316f1b
Eric J. Holmes <eric@ejholmes.net>
2014-01-24 22:03:03 -0800

    Restart the timeout on route change events.

--- src/angular-flash.js
+++ src/angular-flash.js
@@ -3,18 +3,6 @@
 
   var templateUrl = 'template/flash-messages.html';
 
-  /**
-   * See
-   * https://github.com/jashkenas/underscore/blob/2c709d72c89a1ae9e06c56fc256c14435bfa7893/underscore.js#L770
-   */
-  var after = function(times, func) {
-    return function() {
-      if (--times < 1) {
-        return func.apply(this, arguments);
-      }
-    };
-  };
-
   var module = angular.module('ngFlash', ['ng']);
 
   module.provider('$flash', function() {
@@ -60,13 +48,46 @@
       };
 
       /**
+       * Starts the timeout to remove this message. Cancels the existing
+       * timeout if it's present.
+       */
+      FlashMessage.prototype.startTimeout = function() {
+        if (this.timeout) {
+          $timeout.cancel(this.timeout);
+        }
+
+        this.timeout = $timeout(this.remove, this.duration);
+
+        return this.timeout;
+      };
+
+      /**
        * Initialize timeouts.
        */
       FlashMessage.prototype.init = function() {
-        this.timeout = $timeout(this.remove, this.duration);
+        this.startTimeout();
 
         // Remove the flash message when the user navigates.
         if (this.persist) {
+          var _this = this;
+
+          /**
+           * Only runs `func` after the function has been executed `times` times. Each time that `func` is not run,
+           * it will restart the timeout to remove this message.
+           *
+           * See
+           * https://github.com/jashkenas/underscore/blob/2c709d72c89a1ae9e06c56fc256c14435bfa7893/underscore.js#L770
+           */
+          var after = function(times, func) {
+            return function() {
+              if (--times < 1) {
+                return func.apply(this, arguments);
+              } else {
+                _this.startTimeout();
+              }
+            };
+          };
+
           $rootScope.$on(routeChangeSuccess, after(this.persist + 1, this.remove));
         } else {
           $rootScope.$on(routeChangeSuccess, this.remove);
